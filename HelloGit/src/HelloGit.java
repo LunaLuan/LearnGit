9150669d9c1a03fc679845aed6b1481f64cbdb8b
LunaLuan <htluandc@gmail.com>
2017-01-02 20:40:33 +0700

    Test conflict

--- HelloGit/src/HelloGit.java
+++ HelloGit/src/HelloGit.java
@@ -3,7 +3,7 @@ public class HelloGit {
 
 	
 	public static void main(String[] args) {
-		// Dòng này của branch2
+		// Dòng này của branch2, vl
 		System.out.println("Hello Git");
 		// Dòng này của branch2
 	}
